c12ab517fca70ce1482c8dfdf85618de042c3456
Peter Dannemann <pdannemann@talend.com>
2020-05-18 08:22:02 -0400

    add redux devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
@@ -8,7 +8,8 @@ import "./index.css";
 import App from "./containers/App";
 import rootReducer from "./store/reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <React.StrictMode>
